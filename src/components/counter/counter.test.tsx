1ca8cf51f7d1cb74717df79df0fc94ad946b4dec
JunHee <macjjuni@strato.co.kr>
2023-12-11 10:15:19 +0900

    Refactor: 테스트 함수 대체(fireEvent => userEvent)

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -1,5 +1,6 @@
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 
 import CountText from './CountText'
 import Counter from '.'
@@ -29,40 +30,43 @@ describe('<Counter /> Test', () => {
 
   // Start - [Function Test: Up, Down, Clear]
 
-  test('Counter up test', () => {
+  test('Counter up test', async () => {
     renderCounter()
 
     const countEle = screen.getByTestId('counter-text')
     const upButton = screen.getByTestId('counter-up-button')
-    fireEvent.click(upButton) // 카운터 증가
+    const user = userEvent.setup()
+    await user.click(upButton)
 
     const expectedCount = (initValue + 1).toString()
     expect(countEle).toHaveTextContent(expectedCount)
   })
 
-  test('Counter down test', () => {
+  test('Counter down test', async () => {
     renderCounter()
 
     const countTextEle = screen.getByTestId('counter-text')
     const initCountTxt = Number(countTextEle?.textContent) // 카운터 초기값
+    const user = userEvent.setup()
 
     const downButton = screen.getByTestId('counter-down-button')
-    fireEvent.click(downButton) // 카운터 감소
+    await user.click(downButton) // 카운터 감소
 
     const expectedCount = (initCountTxt - 1).toString() // 초기값 - 1
 
     expect(countTextEle).toHaveTextContent(expectedCount)
   })
 
-  test('Counter clear test', () => {
+  test('Counter clear test', async () => {
     renderCounter()
 
     const countTextEle = screen.getByTestId('counter-text')
     const upButton = screen.getByTestId('counter-up-button')
     const clearButton = screen.getByTestId('counter-clear-button')
+    const user = userEvent.setup()
 
-    fireEvent.click(upButton) // 카운터 증가
-    fireEvent.click(clearButton) // 카운터 초기화
+    await user.click(upButton) // 카운터 증가
+    await user.click(clearButton) // 카운터 초기화
 
     const expectedCount = '0' // 초기화 후 결과 카운트
     expect(countTextEle).toHaveTextContent(expectedCount)

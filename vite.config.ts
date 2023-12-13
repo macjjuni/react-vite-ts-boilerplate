fe2d78975e1a1bc8ee7d9f1b915fa90246d42dc2
LeeJunHee <macjjuni@strato.co.kr>
2023-12-13 16:47:55 +0900

    feat: 빌드 압축 적용

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, loadEnv } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import eslint from 'vite-plugin-eslint'
 import react from '@vitejs/plugin-react-swc'
+import { compression } from 'vite-plugin-compression2'
+
 // import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -12,9 +14,13 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       eslint(),
+      compression({
+        include: [/\.(js)$/, /\.(scss)$/],
+        threshold: 2000,
+      }),
       createHtmlPlugin({
         minify: true,
-        entry: 'src/entry/main.tsx',
+        entry: '/src/entry/main.tsx',
         template: '/src/index.html',
         inject: { data: { title: env.VITE_TITLE } },
       }),

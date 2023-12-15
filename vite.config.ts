9dfa96978946c5d1ede91c02282e36b2031faf1e
LeeJunHee <macjjuni@strato.co.kr>
2023-12-15 21:50:36 +0900

    faet: 빌드시 data-testid 속성 삭제 처리

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,21 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 import eslint from 'vite-plugin-eslint'
 import react from '@vitejs/plugin-react-swc'
 import { compression } from 'vite-plugin-compression2'
+import removeAttr from 'react-remove-attr'
 
 // import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  const isProd = env.VITE_MODE === 'production'
+
   return {
     plugins: [
       react(),
       eslint(),
-      compression({
-        include: [/\.(js)$/, /\.(scss)$/],
-        threshold: 2000,
-      }),
+      compression({ include: [/\.(js)$/, /\.(scss)$/], threshold: 2000 }),
+      isProd && removeAttr({ extensions: ['jsx', 'tsx'], attributes: ['data-testid'] }),
       createHtmlPlugin({
         minify: true,
         entry: '/src/entry/main.tsx',
